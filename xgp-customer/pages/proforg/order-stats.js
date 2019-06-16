80d8cfd92c1c54c5717c17ada29381adb2d7e485
new2scala <jiajiwu.icare@gmail.com>
2019-06-16 17:45:08 +0200

    test data for proforg order stats

---
+++ xgp-customer/pages/proforg/order-stats.js
@@ -0,0 +1,70 @@
+// pages/proforg/order-stats.js
+const orderStatsTestData = require('../../utils/org-order-stats-td.js')
+
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad: function (options) {
+    let rawData = orderStatsTestData.testdata;
+
+    console.log('order #', rawData.length);
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage: function () {
+
+  }
+})

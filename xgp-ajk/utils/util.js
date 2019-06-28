a20c571a19afb9a5064f747ca99ab360c567fc2c
new2scala <jiajiwu.icare@gmail.com>
2019-06-28 17:47:52 +0200

    ajk-hk-paytest

--- xgp-ajk/utils/util.js
+++ xgp-ajk/utils/util.js
@@ -99,7 +99,7 @@ let roundPriceArr = function (arr) {
   return arr.map(i => roundPrice(i))
 };
 
-const webappBase = 'https://webapp.wonder4.life';
+const webappBase = 'https://webapp.wonder4.life:8443';
 //const webappBase = 'https://47.98.232.21:8443';
 //const webappBase = 'https://webapp.ajkhealth.com';
 const loginUrl = webappBase + '/wxlogin';

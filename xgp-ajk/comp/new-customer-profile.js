1c5815c10f0ecc74b0d41c783594fd2f96687baa
new2scala <jiajiwu.icare@gmail.com>
2019-07-01 14:49:45 +0200

    new customer profile minor fix

--- xgp-ajk/comp/new-customer-profile.js
+++ xgp-ajk/comp/new-customer-profile.js
@@ -13,6 +13,7 @@ Component({
    * Component initial data
    */
   data: {
+    activeTabIndex: 0,
     newCustomer: {},
     profile: {},
     products: [],
@@ -47,6 +48,76 @@ Component({
     onNewCustomerProfile: function(e) {
       let result = this.getData();
       console.log('[todo] onNewCustomerProfile: ', result)
-    }
+      this.onNewCustomerProfileMock(e);
+    },
+    onNextStep: function(e) {
+      this.setData({ activeTabIndex: 1});
+    },
+
+    updateNewCustomer: function (field, e) {
+      var t = this.data.newCustomer;
+      t[field] = e.detail;
+      this.setData({ newCustomer: t });
+    },
+    onInputUserId: function (e) {
+      this.updateNewCustomer("userid", e)
+    },
+    onInputPassword: function (e) {
+      this.updateNewCustomer("password", e)
+    },
+    onInputPassword2: function (e) {
+      this.updateNewCustomer("password2", e)
+    },
+    onInputUserName: function (e) {
+      this.updateNewCustomer("userName", e)
+    },
+    onInputIdCardNo: function (e) {
+      this.updateNewCustomer("userName", e)
+    },
+    onInputMobile: function (e) {
+      this.updateNewCustomer("mobile", e)
+    },
+    onInputPostAddr: function (e) {
+      this.updateNewCustomer("postAddr", e)
+    },
+    onInputHealthTags: function (e) {
+      this.updateNewCustomer("healthTags", e)
+    },
+    onInputMedicineTags: function (e) {
+      this.updateNewCustomer("medicineTags", e)
+    },
+    setInProgress: function (isInProgress) {
+      this.updateNewCustomer("disabled", { detail: isInProgress });
+      let loadingText = isInProgress ? '添加客户中...' : '';
+      this.updateNewCustomer("loadingText", { detail: loadingText });
+    },
+    onNewCustomerProfileMock: function (e) {
+      let that = this;
+      setTimeout(
+        function () {
+          that.setInProgress(false);
+          // wx.showToast({
+          //   title: `用户添加成功`,
+          //   icon: 'success'
+          // });
+          // Toast.loading({
+          //   duration: 1000,       // 持续展示 toast
+          //   forbidClick: true, // 禁用背景点击
+          //   message: '用户添加成功',
+          //   type: 'success'
+          // });
+          // Toast.loading({
+          //   duration: 1000,       // 持续展示 toast
+          //   forbidClick: true, // 禁用背景点击
+          //   message: '用户添加失败',
+          //   type: 'fail'
+          // });
+          console.log('done onNewCustomerProfileMock')
+        },
+        1000
+      )
+      this.setInProgress(true)
+      console.log('in onNewCustomer: ', this.data.newCustomer)
+    },
   }
 })

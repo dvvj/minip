4c7d07ce6c994cbd1687752d7e3f8b4ef2a1927f
new2scala <jiajiwu.icare@gmail.com>
2019-07-19 15:17:05 +0200

    echarts

--- mock-xgp-ajk/pages/prod/medprof/medprof_main.js
+++ mock-xgp-ajk/pages/prod/medprof/medprof_main.js
@@ -12,6 +12,105 @@ const tabIndices = {
   setting: 4
 };
 
+import * as echarts from '../../../ec-canvas/echarts';
+
+let opt2 = {
+  color: ['#32c5e9', '#67e0e3'],
+  tooltip: {
+    trigger: 'axis',
+    axisPointer: {            // 坐标轴指示器，坐标轴触发有效
+      type: 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
+    },
+    confine: true
+  },
+  legend: {
+    data: ['正面', '热度']
+  },
+  grid: {
+    left: 20,
+    right: 20,
+    bottom: 15,
+    top: 40,
+    containLabel: true
+  },
+  xAxis: [
+    {
+      type: 'value',
+      axisLine: {
+        lineStyle: {
+          color: '#999'
+        }
+      },
+      axisLabel: {
+        color: '#666'
+      }
+    }
+  ],
+  yAxis: [
+    {
+      type: 'category',
+      axisTick: { show: false },
+      data: ['汽车之家', '今日头条', '百度贴吧', '一点资讯', '微信', '微博', '知乎'],
+      axisLine: {
+        lineStyle: {
+          color: '#999'
+        }
+      },
+      axisLabel: {
+        color: '#666'
+      }
+    }
+  ],
+  series: [
+    {
+      name: '正面',
+      type: 'bar',
+      stack: '总量',
+      label: {
+        normal: {
+          show: true
+        }
+      },
+      data: [120, 102, 141, 174, 190, 250, 220],
+      itemStyle: {
+        // emphasis: {
+        //   color: '#32c5e9'
+        // }
+      }
+    },
+    {
+      name: '热度',
+      type: 'bar',
+      label: {
+        normal: {
+          show: true,
+          position: 'inside'
+        }
+      },
+      data: [300, 270, 340, 344, 300, 320, 310],
+      itemStyle: {
+        // emphasis: {
+        //   color: '#37a2da'
+        // }
+      }
+    }
+
+  ]
+};
+var chart = null;
+function initChart(canvas, width, height) {
+  chart = echarts.init(canvas, null, {
+    width: width,
+    height: height
+  });
+  canvas.setChart(chart);
+
+  var option = opt2;
+
+  chart.setOption(option);
+  return chart;
+};
+
 Page({
 
   /**
@@ -25,8 +124,11 @@ Page({
       endYear: 2019,
       endMonth: 7
     },
-    customerInfos: []
+    customerInfos: [],
 
+    ec: {
+      onInit: initChart
+    }
   },
 
 

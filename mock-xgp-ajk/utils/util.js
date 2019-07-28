e3db72e9b2fb4d04238990ebf46522913f7a28cf
new2scala <jiajiwu.icare@gmail.com>
2019-07-28 03:15:24 +0200

    tmp

--- mock-xgp-ajk/utils/util.js
+++ mock-xgp-ajk/utils/util.js
@@ -112,6 +112,7 @@ const webappBase = 'https://webapp.wonder4.life';
 //const webappBase = 'https://47.98.232.21:8443';
 //const webappBase = 'https://webapp.ajkhealth.com';
 const loginUrl = webappBase + '/wxlogin';
+//const loginUrl = webappBase + '/wxloginUidOrMobile';
 const imgBaseUrl = webappBase + '/product';
 const customerBaseUrl = webappBase + '/customer';
 const medprofBaseUrl = webappBase + '/medprof';
